97cb51941c023c87f59ce949f130a052e55e0dbe
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-03 00:25:54 -0200

    feat(Cidade): implementar métodos getters e setters

--- src/main/java/br/com/alugometro/domain/Cidade.java
+++ src/main/java/br/com/alugometro/domain/Cidade.java
@@ -30,5 +30,29 @@ public class Cidade {
 	@ManyToOne
 	@JoinColumn(name = "Id_Estado")
 	private Estado estado;
+
+	public Long getIdCidade() {
+		return idCidade;
+	}
+
+	public void setIdCidade(Long idCidade) {
+		this.idCidade = idCidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
 	
 }
