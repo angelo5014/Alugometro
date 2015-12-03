22f04516d55a90ea44735966570da647f721247c
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-03 02:28:02 -0200

    feat(Teste): adicionar primeiro teste de infraestrutura

--- src/test/java/br/com/alugometro/AbstractInfrastructureTest.java
+++ src/test/java/br/com/alugometro/AbstractInfrastructureTest.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import br.com.alugometro.AppConfig;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(AppConfig.class)
 @WebAppConfiguration
---
+++ src/test/java/br/com/alugometro/dao/TipoImovelDAOTest.java
@@ -0,0 +1,21 @@
+package br.com.alugometro.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.alugometro.AbstractInfrastructureTest;
+import br.com.alugometro.domain.TipoImovel;
+
+public class TipoImovelDAOTest extends AbstractInfrastructureTest {
+
+	@Autowired
+	private TipoImovelDAO tipoImovelDAO;
+	
+	@Test
+	public void deveEncontrarTipoDeImovelPorId() throws Exception {
+		TipoImovel tipoImovel = tipoImovelDAO.encontrarPorId(1L);
+		Assert.assertNotNull(tipoImovel);
+	}
+
+}
