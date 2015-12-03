3b80aaa6ba8f9c8a395dcbb1d26c77630c2d2a92
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-03 00:24:09 -0200

    feat(Cidade): implementar mapeamento da entidade Cidade

---
+++ src/main/java/br/com/alugometro/domain/Cidade.java
@@ -0,0 +1,34 @@
+package br.com.alugometro.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cidade")
+@SequenceGenerator(name = Cidade.SEQUENCE_NAME, sequenceName = Cidade.SEQUENCE_NAME, allocationSize = 1)
+public class Cidade {
+	
+	public static final String SEQUENCE_NAME = "SEQ_Cidade";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@Column(name = "Id_Cidade")
+	private Long idCidade;
+	
+	@Column(name = "Nome", length = 300)
+	@Basic(optional = false)
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "Id_Estado")
+	private Estado estado;
+	
+}
