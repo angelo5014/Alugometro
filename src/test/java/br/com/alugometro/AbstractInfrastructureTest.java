362ebe1f898ec0269994647002977629279d54a4
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-03 01:38:36 -0200

    feat(Tests): implementar classe abstrata para testes com o Spring

---
+++ src/test/java/br/com/alugometro/AbstractInfrastructureTest.java
@@ -0,0 +1,13 @@
+package br.com.alugometro;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(AppConfig.class)
+@WebAppConfiguration
+public class AbstractInfrastructureTest {
+
+}
