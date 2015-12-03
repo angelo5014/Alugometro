9172c8a98e6e07e692059bfbc85968b5e168ae36
Pablo Brodt <pablobrodt@gmail.com>
2015-12-03 15:13:41 -0200

    chore(Security): adicionar classe SecurityConfig

---
+++ src/main/java/br/com/alugometro/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package br.com.alugometro.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+    
+    private final String userQuery = "SELECT Email FROM Usuario WHERE Email = ?";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/css/**", "/js/**", "/vendor/css/**", "/vendor/js/**").permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                //.usersByUsernameQuery(userQuery)
+                .passwordEncoder(new Md5PasswordEncoder());
+    }
+}
+
