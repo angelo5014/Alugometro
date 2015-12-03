02a59e93019828b37de3be9c76277f156df5806c
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-03 19:21:53 -0200

    fix(SecurityConfig): alterar caminho para vendor

--- src/main/java/br/com/alugometro/security/SecurityConfig.java
+++ src/main/java/br/com/alugometro/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/css/**", "/js/**", "/vendor/css/**", "/vendor/js/**").permitAll()
+                .antMatchers("/", "/css/**", "/js/**", "/vendor/**").permitAll()
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin()
