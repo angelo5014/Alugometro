4c2a5d2e1d59ef7b3b6842288aec7a7ea44aabc8
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-12-03 20:49:02 -0200

    test(Anuncio): adicionando testes da service do anuncio

---
+++ src/test/java/br/com/alugometro/service/AnuncioServiceTest.java
@@ -0,0 +1,56 @@
+package br.com.alugometro.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.com.alugometro.dao.AnuncioDAO;
+import br.com.alugometro.domain.Anuncio;
+import br.com.alugometro.domain.Cidade;
+import br.com.alugometro.domain.Estado;
+import br.com.alugometro.domain.Federacao;
+import br.com.alugometro.domain.Foto;
+import br.com.alugometro.domain.TipoAcomodacao;
+import br.com.alugometro.domain.TipoImovel;
+import br.com.alugometro.domain.Usuario;
+import br.com.alugometro.domain.Anuncio.SituacaoAnuncio;
+import br.com.alugometro.mapper.AnuncioMapper;
+
+public class AnuncioServiceTest {
+	
+	@InjectMocks
+	AnuncioService anuncioService;
+	
+	@Mock
+	AnuncioDAO anuncioDAO;
+	
+	Anuncio anuncio = new Anuncio();
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		anuncio.setIdAnuncio(1L);
+		anuncio.setCidade(new Cidade(new Estado(new Federacao(1L))));
+		anuncio.setUsuario(new Usuario());
+		anuncio.setTipoImovel(new TipoImovel());
+		anuncio.setTipoAcomodacao(new TipoAcomodacao());
+		anuncio.setFotoCapa(new Foto());
+		anuncio.setSituacao(SituacaoAnuncio.DISPONIVEL);
+	}
+	
+	@Test
+	public void buscarPorIdnaoEhNulo() {
+		Mockito.when(anuncioDAO.encontrarPorId(1L)).thenReturn(anuncio);
+		Assert.assertNotNull(anuncioService.buscarPorID(1L));
+	}
+	
+	@Test
+	public void inserirAnuncioComSucesso() {
+		Assert.assertNotNull(anuncioService.inserir(AnuncioMapper.paraDTO(anuncio)));
+	}
+	
+}
