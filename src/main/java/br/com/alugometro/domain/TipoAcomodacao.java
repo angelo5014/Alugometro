2d390acfad8a4b984ff11fe56021b7b8dfcdf960
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-12-02 23:52:01 -0200

    feat(TipoAcomodacao): mapeando entidade do Tipo de acomodacao

---
+++ src/main/java/br/com/alugometro/domain/TipoAcomodacao.java
@@ -0,0 +1,24 @@
+package br.com.alugometro.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "TipoAcomodacao")
+public class TipoAcomodacao {
+
+	@Id
+	@Column(name = "IdTipoAcomodacao")
+	private Long tipoAcomodacao;
+	
+	@Column(name = "Descricao")
+	@Length(max = 50)
+	@Basic(optional = false)
+	private String descricao;
+	
+}
