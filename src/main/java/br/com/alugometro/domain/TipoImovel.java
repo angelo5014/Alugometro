e044ee68928a2e27d2495cf9cf9e8b827a11c470
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-12-02 23:52:33 -0200

    feat(TipoImovel): mapeando entidade do Tipo de imóvel

---
+++ src/main/java/br/com/alugometro/domain/TipoImovel.java
@@ -0,0 +1,40 @@
+package br.com.alugometro.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "TipoImovel")
+public class TipoImovel {
+	
+	@Id
+	@Column(name = "IdTipoImovel")
+	private Long idTipoImovel;
+	
+	@Column(name = "Descricao")
+	@Length(max = 50)
+	@Basic(optional = false)
+	private String descricao;
+
+	public Long getIdTipoImovel() {
+		return idTipoImovel;
+	}
+
+	public void setIdTipoImovel(Long idTipoImovel) {
+		this.idTipoImovel = idTipoImovel;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
