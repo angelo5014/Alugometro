9e038279150df18da7ea801890f77cf07e52c3b3
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-05 10:36:28 -0200

    feat(AnuncioDAO): implementar método para buscar por Cidade

--- src/main/java/br/com/alugometro/dao/AnuncioDAO.java
+++ src/main/java/br/com/alugometro/dao/AnuncioDAO.java
@@ -18,6 +18,41 @@ public class AnuncioDAO extends AbstractDAO {
 				.getResultList();
 	}
 	
+	public List<Anuncio> listarPorCidade(String cidade) {
+		return em.createQuery("FROM Anuncio a WHERE a.cidade.nome = :cidade", Anuncio.class)
+				.setParameter("cidade", cidade)
+				.getResultList();
+	}
+	
+	// Exemplo de busca com varios parametros
+	/*
+	public List<Anuncio> listarPorCidade(String material, String servico) {
+		StringBuilder sql = new StringBuilder("FROM Produto p WHERE 1=1");
+
+		HashMap<String, Object> parameters = new HashMap<String, Object>();
+
+		if (material != null) {
+			sql.append(" AND p.material.descricao LIKE :material");
+			parameters.put("material", material);
+		}
+		if (servico != null) {
+			sql.append(" AND p.servico.descricao LIKE :servico");
+			parameters.put("servico", servico);
+		}
+
+		TypedQuery<Produto> query = em.createQuery(sql.toString(), Produto.class);
+
+		Set set = parameters.entrySet();
+		Iterator iterator = set.iterator();
+		while (iterator.hasNext()) {
+			Map.Entry mentry = (Map.Entry) iterator.next();
+			query.setParameter(mentry.getKey().toString(), mentry.getValue() + "%");
+		}
+		
+		return query.getResultList();
+	}
+	*/
+	
 	public Anuncio salvar(Anuncio anuncio) {
 		if(anuncio.getIdAnuncio() == null){
 			em.persist(anuncio);
