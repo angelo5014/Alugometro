81bc35775f04550ec9402e73edbd68598e2f13c2
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-06 15:30:41 -0200

    chore(Inserção): versionar scripts para adicionar fotos

---
+++ SQL/26_adicionar_fotos.sql
@@ -0,0 +1,9 @@
+INSERT INTO ANUNCIO_FOTO (ID_ANUNCIO_FOTO, ID_ANUNCIO, ID_FOTO) VALUES (2, 1, 3);
+INSERT INTO ANUNCIO_FOTO (ID_ANUNCIO_FOTO, ID_ANUNCIO, ID_FOTO) VALUES (3, 1, 4);
+INSERT INTO ANUNCIO_FOTO (ID_ANUNCIO_FOTO, ID_ANUNCIO, ID_FOTO) VALUES (4, 1, 2);
+
+INSERT INTO FOTO (ID_FOTO, URL) VALUES (2, 'http://cdn.plantasdecasas.com/wp-content/uploads/2012/11/Casa-de-Campo-Minas-Gerais-Cod.-400-590x300.jpg');
+INSERT INTO FOTO (ID_FOTO, URL) VALUES (3, 'http://quinan.com/wp-content/uploads/2015/07/fotos-de-casas-modernas-3-2.jpg');
+INSERT INTO FOTO (ID_FOTO, URL) VALUES (4, 'http://www.decoracaoplanejada.com/wp-content/gallery/casas-antigas-decoradas/casas-1.jpg');
+
+commit;
