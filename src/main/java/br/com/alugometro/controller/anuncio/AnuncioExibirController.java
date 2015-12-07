fd42f1742a6209405353f38ff210a27e5be29f16
iagodahlem <iagodahlemlorensini@gmail.com>
2015-12-07 01:17:20 -0200

    feat(DetalharAnuncio): adicionar metodo  para gerar json

--- src/main/java/br/com/alugometro/controller/anuncio/AnuncioExibirController.java
+++ src/main/java/br/com/alugometro/controller/anuncio/AnuncioExibirController.java
@@ -2,11 +2,14 @@ package br.com.alugometro.controller.anuncio;
 
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alugometro.dto.AnuncioFotoDTO;
@@ -21,29 +24,42 @@ import br.com.alugometro.service.TipoImovelService;
 public class AnuncioExibirController extends AbstractAnuncioController {
 
 	private AnuncioFotoService anuncioFotoService;
-	
+
 	@Autowired
-	public AnuncioExibirController(
-			AnuncioService anuncioService,
-			AnuncioFotoService anuncioFotoService,
-			TipoImovelService tipoImovelService,
-			TipoAcomodacaoService tipoAcomodacaoService, 
+	public AnuncioExibirController(AnuncioService anuncioService, AnuncioFotoService anuncioFotoService,
+			TipoImovelService tipoImovelService, TipoAcomodacaoService tipoAcomodacaoService,
 			CidadeService cidadeService) {
-		
+
 		super(anuncioService, tipoImovelService, tipoAcomodacaoService, cidadeService);
-		this.anuncioFotoService = anuncioFotoService; 
+		this.anuncioFotoService = anuncioFotoService;
 	}
-	
-	@RequestMapping(path = "/{idAnuncio}",method=RequestMethod.GET)
+
+	@RequestMapping(path = "/{idAnuncio}", method = RequestMethod.GET)
 	public ModelAndView exibir(@PathVariable("idAnuncio") Long idAnuncio) {
-		
+
 		ModelAndView model = new ModelAndView("anuncio/exibir", "anuncio", anuncioService.buscarPorID(idAnuncio));
 		model.addObject("anuncioFotos", listaFotos(idAnuncio));
 		return model;
 	}
-	
-	private List<AnuncioFotoDTO> listaFotos(Long idAnuncio){
+
+	private List<AnuncioFotoDTO> listaFotos(Long idAnuncio) {
 		return anuncioFotoService.listarPorIdAnuncio(idAnuncio);
+	}
+
+	// REST
+	@ResponseBody
+	@RequestMapping(path = "/rest/{idAnuncio}")
+	public String buscarAnuncioFotos(@PathVariable("idAnuncio") Long idAnuncio) {
+		
+		JSONArray anuncioFotosArray = new JSONArray();
+		
+		for (AnuncioFotoDTO anuncioFotoDTO : listaFotos(idAnuncio)) {
+			JSONObject userJSON = new JSONObject();
+			userJSON.put("urlFoto", anuncioFotoDTO.getUrlFoto());
+			anuncioFotosArray.put(userJSON);
+		}
+		
+		return anuncioFotosArray.toString();
 	}
 	
 }
