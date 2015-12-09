bd019983958f454fc7e6124a9fa1825ffe996934
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-12-09 01:33:30 -0200

    test(Anuncio): adicionando testes

--- src/test/java/br/com/alugometro/service/AnuncioServiceTest.java
+++ src/test/java/br/com/alugometro/service/AnuncioServiceTest.java
@@ -1,6 +1,9 @@
 package br.com.alugometro.service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,6 +19,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import br.com.alugometro.AppConfig;
 import br.com.alugometro.dao.AnuncioDAO;
+import br.com.alugometro.dao.UsuarioDAO;
 import br.com.alugometro.domain.Anuncio;
 import br.com.alugometro.domain.Anuncio.SituacaoAnuncio;
 import br.com.alugometro.domain.Cidade;
@@ -23,6 +27,11 @@ import br.com.alugometro.domain.Foto;
 import br.com.alugometro.domain.TipoAcomodacao;
 import br.com.alugometro.domain.TipoImovel;
 import br.com.alugometro.domain.Usuario;
+import br.com.alugometro.dto.AnuncioDTO;
+import br.com.alugometro.dto.AnuncioResumoDTO;
+import br.com.alugometro.exception.AbstractException;
+import br.com.alugometro.exception.UsuarioNaoEncontradoException;
+import br.com.alugometro.mapper.AnuncioMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AppConfig.class)
@@ -35,6 +44,9 @@ public class AnuncioServiceTest {
 	@Mock
 	AnuncioDAO anuncioDAO;
 	
+	@Mock
+	UsuarioDAO usuarioDAO;
+	
 	Anuncio anuncio = new Anuncio();
 	
 	@Before
@@ -45,6 +57,7 @@ public class AnuncioServiceTest {
 		anuncio.setUsuario(new Usuario());
 		anuncio.setTipoImovel(new TipoImovel());
 		anuncio.setTipoAcomodacao(new TipoAcomodacao());
+		anuncio.setDiaria(new BigDecimal("100"));
 		anuncio.setDataInicio(new Date());
 		anuncio.setDataFim(new Date());
 		anuncio.setFotoCapa(new Foto());
@@ -61,6 +74,63 @@ public class AnuncioServiceTest {
 	public void desativarAnuncio(){
 		Mockito.when(anuncioDAO.desativar(1L)).thenReturn(1);
 		Assert.assertEquals(1, anuncioService.desativarAnuncio(1L));
+	}
+	
+	@Test
+	public void listarTodos(){
+		List<Anuncio> list = new ArrayList<>();
+		list.add(anuncio);
+		
+		Mockito.when(anuncioDAO.listarTodos()).thenReturn(list);
+		
+		Assert.assertEquals(AnuncioMapper.paraDTO(anuncio).getIdAnuncio(), anuncioService.listarTodos().get(0).getIdAnuncio());
+	}
+	
+	@Test
+	public void buscarAnuncioDeUsuario() {
+		List<Anuncio> list = new ArrayList<>();
+		list.add(anuncio);
+		Usuario usuario = new Usuario();
+		usuario.setIdUsuario(1L);
+		usuario.setAnuncios(list);
+		
+		try {
+			Mockito.when(usuarioDAO.buscarPorId(1L)).thenReturn(usuario);
+		} catch (UsuarioNaoEncontradoException e) {
+			e.printStackTrace();
+		}
+		
+		Assert.assertEquals(list.get(0).getIdAnuncio(), anuncioService.buscarAnunciosDoUsuario(1L).get(0).getIdAnuncio());
+		
+	}
+	
+	@Test
+	public void buscarAnuncioDeUsuarioPorEmail() {
+		List<Anuncio> list = new ArrayList<>();
+		list.add(anuncio);
+		Usuario usuario = new Usuario();
+		usuario.setIdUsuario(1L);
+		usuario.setEmail("teste@teste.com");
+		usuario.setAnuncios(list);
+		
+		try {
+			Mockito.when(usuarioDAO.buscarPorEmail("teste@teste.com")).thenReturn(usuario);
+		} catch (AbstractException e) {
+			e.printStackTrace();
+		}
+		
+		Assert.assertEquals(list.get(0).getIdAnuncio(), anuncioService.buscarAnunciosDoUsuarioEmail("teste@teste.com").get(0).getIdAnuncio());
+		
+	}
+	
+	@Test
+	public void listarPorCidade() {
+		List<Anuncio> list = new ArrayList<>();
+		list.add(anuncio);
+		
+		Mockito.when(anuncioDAO.listarPorCidade("São Leopoldo")).thenReturn(list);
+		
+		Assert.assertEquals(new AnuncioResumoDTO(anuncio).getCidade(), anuncioService.listarPorCidade("São Leopoldo").get(0).getCidade());
 	}
 	
 }
